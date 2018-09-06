03eb8da8e673c4c1a643690fd10b5e10ae8f0b8b
Matthew Wheatley <mwheatley@diamondfortress.com>
2018-09-06 09:59:05 -0500

    version 1.0.0 * added package name for publishing * added version constraints for package dependencies

--- package.js
+++ package.js
@@ -1,15 +1,21 @@
 Package.describe({
-    summary: "Login service for PassPrint accounts",
+    name: 'diamondfortress:accounts-passprint',
     version: "1.0.0",
-    git: "https://github.com/DFTinc/atmosphere-accounts-passprint.git"
+    // Brief, one-line summary of the package.
+    summary: "Login service for PassPrint accounts",
+    // URL to the Git repository containing the source code for this package.
+    git: "https://github.com/DFTinc/atmosphere-accounts-passprint.git",
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.use('accounts-base', ['client', 'server']);
+    api.use('accounts-base@1.4.2', ['client', 'server']);
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth', ['client', 'server']);
-    api.use('passprint', ['client', 'server']);
+    api.use('accounts-oauth@1.1.15', ['client', 'server']);
+    api.use('passprint@1.0.0', ['client', 'server']);
 
     api.addFiles('passprint_login_button.css', 'client');
 
